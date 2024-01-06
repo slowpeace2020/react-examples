18e46f4f80a7adb714c0ac87a8c53a1c90ac42bb
slowpeace2020 <slowpeace2020@gmail.com>
2024-01-06 13:23:58 -0500

    js组件大写开头

--- react-staging/src/App.js
+++ react-staging/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//创建外壳组件App
+import React,{Component} from "react";
+import Hello from "./components/Hello/Hello";
+import Welcome from "./components/Welcome/Welcome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//创建并暴露App组件
+export default class App extends Component{
+    render() {
+        return (
+            <div>
+                <Hello/>
+                <Welcome/>
+            </div>
+        )
+    }
 }
 
-export default App;
+//默认暴露,暴露App组件
+// export default App
--- react-staging/src/index.js
+++ react-staging/src/index.js
@@ -1,9 +1,12 @@
+//引入react核心库
 import React from 'react';
+//引入ReactDOM
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//引入App组件，.js可省略
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+//渲染App到页面
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
