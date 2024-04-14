8e00975f6d066e6dffa240201056afe8c6b3f988
shan chu <shanchu@shans-mbp.mynetworksettings.com>
2024-04-13 23:36:21 -0400

    account json server

---
+++ accounts-app/note.md
@@ -0,0 +1,6 @@
+json-server --watch db.json --port 3004
+本地json-server提供数据，作为后端API
+https://mockapi.io/ 做后端API
+
+React快速开发迷你记账簿(共10期)(已完结)
+进度 04 00：00
--- accounts-app/src/components/Records.js
+++ accounts-app/src/components/Records.js
@@ -1,18 +1,32 @@
 import {Component} from "react";
+import Record from "./Record";
 
 class Records extends Component {
+    constructor() {
+        super();
+        this.state ={
+            records:[
+                {"id":1,"date":"2018-09-01","title":"收入","amount":20},
+                {"id":2,"date":"2018-10-01","title":"录视频收入","amount":20},
+                {"id":3,"date":"2018-11-01","title":"广告","amount":20},
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <h2>Records</h2>
                 <table className="table table-bordered">
                     <thead>
-                    <th>
-                        <td>Date</td>
-                        <td>Title</td>
-                        <td>Account</td>
-                    </th>
+                    <tr>
+                        <th>Date</th>
+                        <th>Title</th>
+                        <th>Account</th>
+                    </tr>
                     </thead>
+                    <tbody>
+                    {this.state.records.map((record)=><Record key={record.id} record={record}/>)}
+                    </tbody>
                 </table>
 
             </div>
