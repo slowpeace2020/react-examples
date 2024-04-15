de4083c385879aedc82d82502dd1d6ff1e5489db
shan chu <shanchu@shans-mbp.mynetworksettings.com>
2024-04-15 19:24:00 -0400

    request

--- accounts-app/src/components/Records.js
+++ accounts-app/src/components/Records.js
@@ -1,18 +1,55 @@
 import {Component} from "react";
 import Record from "./Record";
+// import $ from 'jquery';
+import {getJSON} from 'jquery';
+import axios from 'axios';
 
 class Records extends Component {
     constructor() {
         super();
-        this.state ={
-            records:[
-                {"id":1,"date":"2018-09-01","title":"收入","amount":20},
-                {"id":2,"date":"2018-10-01","title":"录视频收入","amount":20},
-                {"id":3,"date":"2018-11-01","title":"广告","amount":20},
-            ]
+        this.state = {
+            error: null,
+            isLoaded: false,
+            records: []
         }
     }
+
+    componentDidMount() {
+        // $.getJSON("https://661b353e65444945d04f27a0.mockapi.io/api/v1/records").then(
+        // getJSON("https://661b353e65444945d04f27a0.mockapi.io/api/v1/records").then(
+            // response=>console.log(response),
+            // response=>console.log(response),
+        //     response => this.setState({
+        //         records: response,
+        //         isLoaded: true,
+        //     }),
+        //     error => this.setState({
+        //         isLoaded: true,
+        //         error
+        //     })
+        // )
+        axios.get("https://661b353e65444945d04f27a0.mockapi.io/api/v1/records").then(
+            // response=>console.log(response),
+            // response=>console.log(response),
+            response => this.setState({
+                records: response,
+                isLoaded: true,
+            }),
+        ).catch(
+            error => this.setState({
+                isLoaded: true,
+                error
+            })
+        )
+    }
+
     render() {
+        const {error,isLoaded, records} = this.state;
+        if (error){
+            return <div>Error:{error.responseText}</div>
+        }else if(!isLoaded){
+            return <div>Loading....</div>
+        }
         return (
             <div>
                 <h2>Records</h2>
@@ -25,7 +62,7 @@ class Records extends Component {
                     </tr>
                     </thead>
                     <tbody>
-                    {this.state.records.map((record)=><Record key={record.id} record={record}/>)}
+                    {records.map((record) => <Record key={record.id} {...record}/>)}
                     </tbody>
                 </table>
 
